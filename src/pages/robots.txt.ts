a9b5fab6ef9765851b4cdf7ff445aa20dc67d2a7
Tedd Schreiner <t.schreiner@eos-ts.com>
2026-07-05 13:35:32 +0200

    Add CSP headers and enable indexing

--- src/pages/robots.txt.ts
+++
@@ -1,9 +0,0 @@
-import { site } from '@lib/site';
-
-export function GET() {
-  return new Response(`User-agent: *\nAllow: /\n\nSitemap: ${site.url}/sitemap.xml\n`, {
-    headers: {
-      'Content-Type': 'text/plain; charset=utf-8',
-    },
-  });
-}
